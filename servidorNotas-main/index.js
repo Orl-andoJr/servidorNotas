1cb5333c06c1ae1560e3a58bafec0de6f57c559a
Orlando Jr <112992075+Orl-andoJr@users.noreply.github.com>
2023-01-30 21:29:28 -0300

    edit da Farmácia

---
+++ servidorNotas-main/index.js
@@ -0,0 +1,10 @@
+// const jsonServer = require('json-server')
+import jsonServer from "json-server";
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3200;
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
